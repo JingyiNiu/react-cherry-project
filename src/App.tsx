82a6285cbccb637ebb591fee9127008361f33a95
JingyiNiu <niu.jy1203@gmail.com>
2021-07-21 12:24:51 +1200

    install react-router-dom and set up basic routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import Nav from "./components/nav/nav.component";
 import Footer from "./components/footer/footer.component";
+
+import Home from "./pages/home/home.page";
+import Products from "./pages/products/products.page";
 
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Nav />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/products' component={Products} />
+      </Switch>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
