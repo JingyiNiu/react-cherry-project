1a01a5fa282a30438fe794c95520283717b3b289
JingyiNiu <niu.jy1203@gmail.com>
2021-07-27 21:40:51 +1200

    style form input

--- src/components/form/form.component.tsx
+++ src/components/form/form.component.tsx
@@ -8,6 +8,7 @@ const Form = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm();
 
@@ -22,109 +23,124 @@ const Form = () => {
         {/* First Column */}
         <Grid item xs={6}>
           {/* Product Name */}
-          <div className='label'>
-            Product Name <span className='required'>*</span>
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("productName", {
+                required: true,
+              })}
+            />
+            <div className={`${watch("productName") ? "shrink" : ""} label`}>
+              Product Name <span className='required'>*</span>
+            </div>
           </div>
-          <input
-            className='input'
-            placeholder='Product Name'
-            {...register("productName", {
-              required: true,
-            })}
-          />
           {errors?.productName?.type === "required" && (
             <p className='message'>This field is required</p>
           )}
 
           {/* Product Code */}
           <div className='input-group'>
-            <div className='label'>Product Code</div>
-            <input
-              className='input'
-              placeholder='Product Code'
-              {...register("productCode")}
-            />
+            <input className='input' {...register("productCode")} />
+            <div className={`${watch("productCode") ? "shrink" : ""} label`}>
+              Product Code
+            </div>
           </div>
 
           {/* Description */}
-          <div className='label'>Description</div>
-          <input
-            className='input'
-            placeholder='Description'
-            {...register("description")}
-          />
+          <div className='input-group'>
+            <input className='input' {...register("description")} />
+            <div className={`${watch("description") ? "shrink" : ""} label`}>
+              Description
+            </div>
+          </div>
 
           {/* Image URL */}
-          <div className='label'>Image URL</div>
-          <input
-            className='input'
-            placeholder='Image URL'
-            {...register("imageUrl")}
-          />
+          <div className='input-group'>
+            <input className='input' {...register("imageUrl")} />
+            <div className={`${watch("imageUrl") ? "shrink" : ""} label`}>
+              Image URL
+            </div>
+          </div>
         </Grid>
 
         {/* Second Column */}
         <Grid item xs={3}>
           {/* Length (cm) */}
-          <div className='label'>Length (cm)</div>
-          <input
-            className='input'
-            placeholder='Length'
-            {...register("length", {
-              pattern: /^[0-9]+$/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("length", {
+                pattern: /^[0-9]+$/,
+              })}
+            />
+            <div className={`${watch("length") ? "shrink" : ""} label`}>
+              Length (cm)
+            </div>
+          </div>
           {errors?.length?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
 
           {/* Width (cm) */}
-          <div className='label'>Width (cm)</div>
-          <input
-            className='input'
-            placeholder='Width'
-            {...register("width", {
-              pattern: /^[0-9]/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("width", {
+                pattern: /^[0-9]/,
+              })}
+            />
+            <div className={`${watch("width") ? "shrink" : ""} label`}>
+              Width (cm)
+            </div>
+          </div>
           {errors?.width?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
 
           {/* Height (cm) */}
-          <div className='label'>Height (cm)</div>
-          <input
-            className='input'
-            placeholder='Height'
-            {...register("height", {
-              pattern: /^[0-9]+$/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("height", {
+                pattern: /^[0-9]+$/,
+              })}
+            />
+            <div className={`${watch("height") ? "shrink" : ""} label`}>
+              Height (cm)
+            </div>
+          </div>
           {errors?.height?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
+
           {/* Weight (kg) */}
-          <div className='label'>Weight (kg)</div>
-          <input
-            className='input'
-            placeholder='Weight'
-            {...register("weight", {
-              pattern: /^[0-9]+$/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("weight", {
+                pattern: /^[0-9]+$/,
+              })}
+            />
+            <div className={`${watch("weight") ? "shrink" : ""} label`}>
+              Weight (kg)
+            </div>
+          </div>
           {errors?.weight?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
 
           {/* Package Quantity */}
-          <div className='label'>Package Quantity</div>
-          <input
-            className='input'
-            placeholder='Package Quantity'
-            {...register("packageQty", {
-              pattern: /^[0-9]+$/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("packageQty", {
+                pattern: /^[0-9]+$/,
+              })}
+            />
+            <div className={`${watch("packageQty") ? "shrink" : ""} label`}>
+              Package Quantity
+            </div>
+          </div>
           {errors?.packageQty?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
@@ -133,88 +149,110 @@ const Form = () => {
         {/* Third Column */}
         <Grid item xs={3}>
           {/* Price */}
-          <div className='label'>Price</div>
-          <input
-            className='input'
-            placeholder='Price'
-            {...register("price", {
-              pattern: /^[0-9]+$/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("price", {
+                pattern: /^[0-9]+$/,
+              })}
+            />
+            <div className={`${watch("price") ? "shrink" : ""} label`}>
+              Price
+            </div>
+          </div>
           {errors?.price?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
 
           {/* RRP Price */}
-          <div className='label'>RRP Price</div>
-          <input
-            className='input'
-            placeholder='RRP Price'
-            {...register("priceRrp", {
-              pattern: /^[0-9]+$/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("priceRrp", {
+                pattern: /^[0-9]+$/,
+              })}
+            />
+            <div className={`${watch("priceRrp") ? "shrink" : ""} label`}>
+              RRP Price
+            </div>
+          </div>
           {errors?.priceRrp?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
 
           {/* Agent Price */}
-          <div className='label'>Agent Price</div>
-          <input
-            className='input'
-            placeholder='Agent Price'
-            {...register("priceAgent", {
-              pattern: /^[0-9]+$/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("priceAgent", {
+                pattern: /^[0-9]+$/,
+              })}
+            />
+            <div className={`${watch("priceAgent") ? "shrink" : ""} label`}>
+              Agent Price
+            </div>
+          </div>
           {errors?.priceAgent?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
 
           {/* Shopify Price */}
-          <div className='label'>Shopify Price</div>
-          <input
-            className='input'
-            placeholder='Shopify Price'
-            {...register("priceShopify", {
-              pattern: /^[0-9]+$/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("priceShopify", {
+                pattern: /^[0-9]+$/,
+              })}
+            />
+            <div className={`${watch("priceShopify") ? "shrink" : ""} label`}>
+              Shopify Price
+            </div>
+          </div>
           {errors?.priceShopify?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
 
           {/* 12-12 Price */}
-          <div className='label'>12-12 Price</div>
-          <input
-            className='input'
-            placeholder='12-12 Price'
-            {...register("price1212", {
-              pattern: /^[0-9]+$/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("price1212", {
+                pattern: /^[0-9]+$/,
+              })}
+            />
+            <div className={`${watch("price1212") ? "shrink" : ""} label`}>
+              12-12 Price
+            </div>
+          </div>
           {errors?.price1212?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
 
           {/* Special Price */}
-          <div className='label'>Special Price</div>
-          <input
-            className='input'
-            placeholder='Special Price'
-            {...register("priceSpecial", {
-              pattern: /^[0-9]+$/,
-            })}
-          />
+          <div className='input-group'>
+            <input
+              className='input'
+              {...register("priceSpecial", {
+                pattern: /^[0-9]+$/,
+              })}
+            />
+            <div className={`${watch("priceSpecial") ? "shrink" : ""} label`}>
+              Special Price
+            </div>
+          </div>
           {errors?.priceSpecial?.type === "pattern" && (
             <p className='message'>Numbers only</p>
           )}
         </Grid>
+
+        {/* Buttons */}
         <div className='button-group products-btn-group'>
           <button className='button btn-primary' type='submit'>
             Submit
           </button>
-          <button className='button btn-gray'>Reset</button>
+          <button className='button btn-gray' type='reset'>
+            Reset
+          </button>
         </div>
       </Grid>
     </form>
